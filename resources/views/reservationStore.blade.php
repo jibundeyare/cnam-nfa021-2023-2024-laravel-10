59d3342b1eccb084d1dd2264a49d7de67a875fc5
jibundeyare <contact@jibundeyare.com>
2024-02-09 10:02:04 +0100

    Confirmation de réservation
    Affichage de la date de réservation au format français.
    Affichage des commentaires seulement s'il y en a.

--- resources/views/reservationStore.blade.php
+++ resources/views/reservationStore.blade.php
@@ -9,9 +9,12 @@
 <ul>
     <li>{{ $reservation->nom }}</li>
     <li>{{ $reservation->couverts }}</li>
-    <li>{{ $reservation->jour->format('d/m/Y') }}</li>
+    {{-- convertit une date au format Y-m-d en date au format d/m/Y --}}
+    <li>{{ date('d/m/Y', strtotime($reservation->jour)) }}</li>
     <li>{{ $reservation->heure }}</li>
     <li>{{ $reservation->telephone }}</li>
+    @if ($reservation->commentaires)
     <li>{{ $reservation->commentaires }}</li>
+    @endif
 </ul>
 @endsection
